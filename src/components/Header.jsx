b256e887270bce0f5344d4ea03d86f7c626a51ac
sayalipatil86-prog <sayalip547@gmail.com>
2026-08-26 16:34:02 +0530

    Remove announcement info bar from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
 import TopHeader from './TopHeader'
-import CollegeInfoBar from './CollegeInfoBar'
 import Navbar from './Navbar'
 
 export default function Header() {
   return (
     <>
       <TopHeader />
-      <CollegeInfoBar />
       <Navbar />
     </>
   )
